641e533544c9c93ef37568481ac59d7d4d72ba14
MaxRayskiy <mraiskii@adalisk.com>
2022-07-01 15:04:52 +0300

    add changelog

---
+++ parse_notes.py
@@ -0,0 +1,11 @@
+with open("input.txt", "r") as input, open("output.txt", "w") as output:
+    skipPrint = True
+    for line in input:
+        line = line.strip()
+        if "What's Changed" in line:
+            skipPrint = False
+        if not line or "**Full Changelog**" in line :
+            continue
+        if not skipPrint:
+            line = line.split(" in https:")[0]
+            output.write(line + "\n")
