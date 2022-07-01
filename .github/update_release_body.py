dcf56862f3d48d32a300a6affb478508e6bea338
MaxRayskiy <mraiskii@adalisk.com>
2022-07-01 16:57:34 +0300

    fix

--- .github/update_release_body.py
+++ .github/update_release_body.py
@@ -20,6 +20,8 @@ with open("changelog.txt", "r") as changelog, open("release_body.txt", "r") as r
         line = line.strip()
         if "What's Changed" in line:
             skipPrint = False
+            output.write(line + "\n\n")
+            continue
         if not line or "**Full Changelog**" in line :
             continue
         if not skipPrint:
